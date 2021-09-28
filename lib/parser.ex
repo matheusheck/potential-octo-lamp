12e5b1eb89fef18e95646e59de1da63dbb96e25c
matheusheck <matheus@conduite.tv>
2021-09-28 17:46:33 +0200

    create parser

---
+++ lib/parser.ex
@@ -0,0 +1,14 @@
+defmodule GenReport.Parser do
+  def parse_file(filename) do
+    filename
+    |> File.stream!()
+    |> Stream.map(&parse_line(&1))
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
+  end
+end
