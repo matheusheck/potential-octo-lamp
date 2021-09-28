29c5e309d20311cc3e032564dcc9f69bd7da5a68
matheusheck <matheus@conduite.tv>
2021-09-28 17:47:07 +0200

    change return to expected struct

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -14,14 +14,47 @@ defmodule GenReport do
     "Vinicius"
   ]
 
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  @report %{
+    all_hours: %{},
+    hours_per_month: %{},
+    hours_per_year: %{}
+  }
+
   def build(filename) do
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn line, report -> total_hours(line, report) end)
+    |> get_hours()
   end
 
-  defp total_hours([name, hours, _day, _month, _year], report),
-    do: Map.put(report, name, report[name] + hours)
-
   defp report_acc, do: Enum.into(@freelancers, %{}, &{&1, 0})
+
+  defp get_hours(parsed_file) do
+    %{
+      all_hours: all_hours(parsed_file),
+      hours_per_month: "hours_per_month",
+      hours_per_year: "hours_per_year"
+    }
+  end
+
+  defp total_hours([name, hours, _day, _month, _year], report) do
+    Map.put(report, name, report[name] + hours)
+  end
+
+  defp all_hours(parsed_file),
+    do: Enum.reduce(parsed_file, report_acc(), fn line, report -> total_hours(line, report) end)
 end
