07f1072b204d4c7e77a7906f86e14d92a7b519f2
matheusheck <matheus@conduite.tv>
2021-09-28 16:09:00 +0200

    add total hours report

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,27 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  @freelancers [
+    "Daniele",
+    "Mayk",
+    "Giuliano",
+    "Cleiton",
+    "Jakeliny",
+    "Joseph",
+    "Diego",
+    "Rafael",
+    "Danilo",
+    "Vinicius"
+  ]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> total_hours(line, report) end)
   end
+
+  defp total_hours([name, hours, _day, _month, _year], report),
+    do: Map.put(report, name, report[name] + hours)
+
+  defp report_acc, do: Enum.into(@freelancers, %{}, &{&1, 0})
 end
