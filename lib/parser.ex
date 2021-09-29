b618375fac1f2f9f9ee7ec283483132dc549b2dd
matheusheck <matheus@conduite.tv>
2021-09-29 23:08:18 +0200

    fix month parser

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,4 +1,19 @@
 defmodule GenReport.Parser do
+  @months %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
+
   def parse_file(filename) do
     filename
     |> File.stream!()
@@ -11,7 +26,13 @@ defmodule GenReport.Parser do
     |> String.split(",")
     |> List.update_at(1, &String.to_integer/1)
     |> List.update_at(2, &String.to_integer/1)
-    |> List.update_at(3, &String.to_integer/1)
+    |> List.update_at(3, &name_month(&1))
     |> List.update_at(4, &String.to_integer/1)
+  end
+
+  defp name_month(month_number) do
+    banana = String.to_integer(month_number)
+    month_name = @months[banana]
+    month_name
   end
 end
