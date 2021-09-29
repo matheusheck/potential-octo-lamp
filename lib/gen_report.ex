e6f8fb61a48dbc21b8ac558550acfc6b1f2fa428
matheusheck <matheus@conduite.tv>
2021-09-29 21:18:14 +0200

    get month works

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -14,20 +14,20 @@ defmodule GenReport do
     "Vinicius"
   ]
 
-  @months [
-    "janeiro",
-    "fevereiro",
-    "março",
-    "abril",
-    "maio",
-    "junho",
-    "julho",
-    "agosto",
-    "setembro",
-    "outubro",
-    "novembro",
-    "dezembro"
-  ]
+  @months %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
 
   @report %{
     all_hours: %{},
@@ -42,13 +42,14 @@ defmodule GenReport do
   end
 
   defp freelancers_acc, do: Enum.into(@freelancers, %{}, &{&1, 0})
-  defp month_acc, do: Enum.into(@months, %{}, &{&1, 0})
+  defp month_acc, do: Enum.into(@freelancers, %{}, &{&1, month_list()})
+  defp month_list, do: Enum.into(1..12, %{}, &{&1, 0})
   # defp years_acc, do: Enum.into(2016..2020, %{}, &{&1, 0})
 
   defp get_hours(parsed_file) do
     %{
       all_hours: all_hours(parsed_file),
-      hours_per_month: "hours_per_month",
+      hours_per_month: month_hours(parsed_file),
       hours_per_year: "hours_per_year"
     }
   end
@@ -58,10 +59,21 @@ defmodule GenReport do
   end
 
   defp all_hours(parsed_file) do
-    Enum.reduce(parsed_file, freelancers_acc(), fn line, report -> total_hours(line, report) end)
+    Enum.reduce(parsed_file, freelancers_acc(), fn line, report_acc ->
+      total_hours(line, report_acc)
+    end)
+  end
+
+  defp total_month_hours([name, hours, _day, month, _year], month_acc) do
+    freela = month_acc[name]
+
+    new_hours = Map.put(freela, month, freela[month] + hours)
+    Map.put(month_acc, name, new_hours)
   end
 
   defp month_hours(parsed_file) do
-    Enum.reduce(parsed_file, freelancers_acc(), fn line, report -> total_hours(line, report) end)
+    Enum.reduce(parsed_file, month_acc(), fn line, month_acc ->
+      total_month_hours(line, month_acc)
+    end)
   end
 end
